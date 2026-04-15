1dc39f774d9b02cb311f449a75daf368e0542b0c
Ish004 <s4008484@student.rmit.edu.au>
2026-04-15 11:48:56 +0800

    AlarmView 1

--- timekeeper/ContentView.swift
+++ timekeeper/ContentView.swift
@@ -9,16 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List{
+                VStack(alignment: .leading, spacing: 0){
+                    HStack(){
+                        Image(systemName: "bed.double.fill")
+                        Text("Sleep | Wake Up").font(.title2) .bold(true)
+                    }
+                    HStack(){
+                        VStack(alignment: .leading){
+                            Text("07.00")
+                                .font(.system(size: 60, weight: .light))
+                            
+                            Text("Tomorrow Morning")
+                        }
+                        Spacer()
+                        Button{} label: {
+                            Text("CHANGE")
+                                .foregroundColor(.orange)
+                                .padding(.vertical, 8)
+                                .padding(.horizontal, 12)
+                                .background(.ultraThinMaterial)
+                                .background(.gray.opacity(0.1))
+                                .cornerRadius(24)
+                                .font(.system(size: 16, weight: .bold))
+                        }
+                    }
+                }
+                
+                VStack(alignment: .leading, spacing: 0){
+                    HStack(){
+                        Text("Other").font(.title2) .bold(true)
+                    }
+                    eachAlarm(time: "08.00", toggle: true)
+                    eachAlarm(time: "14.00", toggle: false)
+                    eachAlarm(time: "08.00", toggle: false)
+                    eachAlarm(time: "14.00", toggle: true)
+                    
+                }
+            }.listStyle(.plain)
         }
-        .padding()
+        .navigationBarTitleDisplayMode(.large)
+        
+        TabView{
+            Tab("World Clock", systemImage: "globe"){
+            }
+            Tab("Alarms", systemImage: "alarm"){
+            }
+            Tab("Stopwatch", systemImage: "stopwatch"){
+            }
+            Tab("Timers", systemImage: "timer"){
+            }
+        }.tint(.orange)
     }
+}
+
+func eachAlarm(time: String, toggle: Bool) -> some View {
+    VStack(alignment: .leading, spacing: 0){
+            HStack(){
+                Text(time)
+                    .font(.system(size: 60, weight: .light))
+                Spacer()
+                Toggle("", isOn: .constant(toggle))
+            }
+        Text("Alarm, everyday")
+        }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
