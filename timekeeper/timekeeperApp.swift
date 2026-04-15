5a48a8f2a6b43b0058b84bfb07b3597fb4355568
Javohir Muhammad <javohirmx@10-64-53-243.0200.binb.id.iosda.org>
2026-04-15 10:44:00 +0800

    chore: init swiftui ios app

---
+++ timekeeper/timekeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  timekeeperApp.swift
+//  timekeeper
+//
+//  Created by Javohir Muhammad on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct timekeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
